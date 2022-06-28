2531a185db9561d2c284fee8859e74d474cac165
Astonlix <107379616+Astonlix@users.noreply.github.com>
2022-06-28 18:32:55 -0500

    Delete #1 Key System.lua

--- #1 Key System.lua
+++
@@ -1,9 +0,0 @@
---[[Features
-AutoTap
-AutoRebirth
-AutoEgg
-]]
-
-
-_G.ReplaceKey = true --Key is Lunar
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Astonlix/Roblox-Lua/main/%231.lua"))()

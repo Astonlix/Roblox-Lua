038a2176ebae2c28c3641a09a79db1c893bd4a06
Astonlix <107379616+Astonlix@users.noreply.github.com>
2022-06-28 18:28:56 -0500

    #1 With Key System
    I will update this script soon with #2.

---
+++ #1 Key System.lua
@@ -0,0 +1,9 @@
+--[[Features
+AutoTap
+AutoRebirth
+AutoEgg
+]]
+
+
+_G.ReplaceKey = true --Key is Lunar
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Astonlix/Roblox-Lua/main/%231.lua"))()
